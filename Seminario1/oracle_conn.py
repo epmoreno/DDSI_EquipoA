783f10175408c25fabda81c79039124548192fc7
Enrique <epmoreno@correo.ugr.es>
2025-11-14 20:05:57 +0100

    Update - Correccion de código

--- Seminario1/oracle_conn.py
+++ Seminario1/oracle_conn.py
@@ -121,33 +121,33 @@ def create_Pedido(ccliente, fecha_pedido):
         return None
 
 def check_tables():
-    print("\n| \tCproducto\t| \tCantidad \t|\n")
+    print("\n| \tCproducto          |       Cantidad        \t\t|\n")
     if cursor:
         cursor.execute("SELECT * FROM Stock")
         productos = cursor.fetchall()
         for producto in productos:
-            print(f"|  - Cproducto: {producto[0]}, Cantidad disponible: {producto[1]}\t|")
+            print(f"|  \t- Cproducto: {producto[0]}     |       Cantidad disponible: {producto[1]}\t|")
     
         print("")
     else:
         print("No se pudo mostrar las tablas debido a problemas de cursor.")
 
-    print("\n| \tCpedido\t| \tCcliente\t| \tFecha_Pedido\t|\n")
+    print("\n| \tCpedido\t\t|       Ccliente\t|       Fecha_Pedido\t\t\t\t|\n")
     if cursor:
         cursor.execute("SELECT * FROM Pedido")
         pedidos = cursor.fetchall()
         for pedido in pedidos:
-            print(f"|  - Cpedido: {pedido[0]}, Ccliente: {pedido[1]}, Fecha_Pedido: {pedido[2]}\t|")
+            print(f"|  \t- Cpedido: {pedido[0]}\t|       Ccliente: {pedido[1]}\t|       Fecha_Pedido: {pedido[2]}\t|")
         print("")
     else:
         print("No se pudo mostrar las tablas debido a problemas de cursor.")
 
-    print("\n| \tCpedido\t| \tCproducto\t| \tCantidad\t|\n")
+    print("\n| \tCpedido\t\t|       Cproducto\t|       Cantidad\t|\n")
     if cursor:
         cursor.execute("SELECT * FROM Detalle_Pedido")
         detalles = cursor.fetchall()
         for detalle in detalles:
-            print(f"|  - Cpedido: {detalle[0]}, Cproducto: {detalle[1]}, Cantidad: {detalle[2]}\t|")
+            print(f"|  \t- Cpedido: {detalle[0]}\t|       Cproducto: {detalle[1]}\t|       Cantidad: {detalle[2]}\t|")
         print("")
     else:
         print("No se pudo mostrar las tablas debido a problemas de cursor.")
@@ -190,7 +190,6 @@ if conn:
                             
                     # SAVEPOINT antes de insertar en Detalle_Pedido y OPCIONES de ejecucion
                     if conn and cursor:
-                        cursor.execute("SAVEPOINT antes_detalle_pedido")
                         try:
                             while flag_in_opcion == False:
                                 opciones_input = int(input("\nINGRESE UNA OPCION: \n  1 - Añadir detalle de producto \n  2 - Eliminar detalles de producto \n  3 - Cancelar Pedido \n  4 - Finalizar Pedido \n\nRespuesta: "))
@@ -212,7 +211,8 @@ if conn:
                                             cursor.execute("SELECT Cproducto, Cantidad FROM Stock")
                                             productos = cursor.fetchall()
                                             for producto in productos:
-                                                print(f"  - Cproducto: {producto[0]}, Cantidad disponible: {producto[1]}")
+                                                print(f"|  \t- Cproducto: {producto[0]}\t|       Cantidad disponible: {producto[1]}\t|")
+                                        
                                             print("")
 
                                             # Introducir y Verificar si el producto existe
@@ -239,41 +239,30 @@ if conn:
                                             else:
                                                 print("Cantidad disponible en stock.")
 
-                                            # Verifica si el producto ya está en Detalle_Pedido
-                                            cursor2 = conn.cursor()
-                                            cursor2.execute("SELECT COUNT(*) FROM Detalle_Pedido WHERE Cproducto = :1", [articulo])
-                                            if cursor2.fetchone()[0] > 0:
+                                            # Verifica si el producto ya está en Detalle_Pedido (por pedido y producto)
+                                            cursor.execute("SELECT COUNT(*) FROM Detalle_Pedido WHERE Cpedido = :1 AND Cproducto = :2", [id_pedido, articulo])
+                                            if cursor.fetchone()[0] > 0:
                                                 # Actualizar Cantidad en Detalle_Pedido
-                                                cursor.execute("SELECT Cpedido FROM Pedido WHERE Ccliente = :1 ORDER BY Fecha_Pedido DESC", [ccliente])
-                                                pedido_id = cursor.fetchone()[0]
-                                                cursor2.execute("UPDATE Detalle_Pedido SET Cantidad = Cantidad + :1 WHERE Cpedido = :3 AND Cproducto = :2", [cantidad_solicitada, articulo, pedido_id])
-                                                #conn.commit()
+                                                print(f"Actualizando cantidad del detalle de pedido...{id_pedido}, {articulo}, {cantidad_solicitada}")
+                                                cursor.execute("UPDATE Detalle_Pedido SET Cantidad = Cantidad + :1 WHERE Cpedido = :2 AND Cproducto = :3", [cantidad_solicitada, id_pedido, articulo])
                                                 print("Cantidad actualizada en Detalle_Pedido.")
                                                 # Actualizar Stock
                                                 cursor.execute("UPDATE Stock SET Cantidad = Cantidad - :1 WHERE Cproducto = :2", [cantidad_solicitada, articulo])
-                                                #conn.commit()
                                                 print("Detalle de pedido añadido exitosamente.")
                                             else:
                                                 # Insertar en Detalle_Pedido
-                                                cursor.execute("SELECT Cpedido FROM Pedido WHERE Ccliente = :1 ORDER BY Fecha_Pedido DESC", [ccliente])
-                                                pedido_id = cursor.fetchone()[0]
-                                                cursor.execute("INSERT INTO Detalle_Pedido (Cpedido, Cproducto, Cantidad) VALUES (:1, :2, :3)", [pedido_id, articulo, cantidad_solicitada])
-                                                #conn.commit()
+                                                print(f"Insertando nuevo detalle de pedido...{id_pedido}, {articulo}, {cantidad_solicitada}")
+                                                cursor.execute("INSERT INTO Detalle_Pedido (Cpedido, Cproducto, Cantidad) VALUES (:1, :2, :3)", [id_pedido, articulo, cantidad_solicitada])
                                                 # Actualizar Stock
                                                 cursor.execute("UPDATE Stock SET Cantidad = Cantidad - :1 WHERE Cproducto = :2", [cantidad_solicitada, articulo])
-                                                #conn.commit()
                                                 print("Detalle de pedido añadido exitosamente.")
                                         except Exception as e:
                                             print(f"Error al recuperar productos de Stock: {e}")
                                     if opciones_input == 2:
                                         try:
-                                            #Eliminar Detalles de pedido en Detalle_Pedido (HACER UN ROLLBACK A SAVEPOINT ANTES_DETALLE_PEDIDO???)
-                                            cursor.execute("ROLLBACK TO antes_detalle_pedido")
-                                            conn.rollback()
-                                            print("Detalles de pedido eliminados exitosamente mediante ROLLBACK al savepoint.")
-
-                                            check_tables()
-                                            
+                                            #Eliminar Detalles de pedido en Detalle_Pedido
+                                            cursor.execute("DELETE FROM Detalle_Pedido WHERE Cpedido = :1", [id_pedido])
+                                            print("Detalles de pedido eliminados exitosamente")
                                         except Exception as e:
                                             print(f"Error al eliminar detalle de pedido: {e}")
                                     # Opciones 3 y 4 que salen del bucle y regresan al menú principal
